790c5b15ae2037bbc8016429b7523410212468a4
kaji0913 <y.kajiyama.1211@gmail.com>
2021-05-30 12:04:59 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before _action :basic_auth
+
+  private 
+  def basic_auth
+   authenticate_or_request_with_http_basic do |username, password|
+    username == 'yoheikajiyama' && password == '19920913'
+   end
+  end
+
 end
