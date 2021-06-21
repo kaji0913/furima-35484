695d33a82fee8b06cbf70d362456e0b380104c96
kaji0913 <y.kajiyama.1211@gmail.com>
2021-06-21 19:23:33 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :last_name_reading, format: {with: /\A[ァ-ヶ]+\z/}
+    validates :first_name_reading, format: {with: /\A[ァ-ヶ]+\z/}
+    validates :birthday
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX
+  end
+
+  has_many :items
+  has_many :orders
 end
