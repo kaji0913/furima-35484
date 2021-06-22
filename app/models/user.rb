115752ef5a11d4d7b478a13057f76332ad50833b
kaji0913 <y.kajiyama.1211@gmail.com>
2021-06-22 14:50:09 +0900

    修正②

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,17 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-    validates :last_name_reading, format: {with: /\A[ァ-ヶー]+\z/}
-    validates :first_name_reading, format: {with: /\A[ァ-ヶー]+\z/}
+    
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      validates :last_name
+      validates :first_name
+    end
+    
+    with_options format: {with: /\A[ァ-ヶー]+\z/} do
+      validates :last_name_reading
+      validates :first_name_reading
+    end
+    
     validates :birthday
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX
